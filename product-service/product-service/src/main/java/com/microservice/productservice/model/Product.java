61bb6284681380a6821d735126b161186470f722
Ahmetkcak <ahmet.aliberk07@gmail.com>
2024-02-27 17:53:57 +0300

    fix:change product id type

--- product-service/product-service/src/main/java/com/microservice/productservice/model/Product.java
+++ product-service/product-service/src/main/java/com/microservice/productservice/model/Product.java
@@ -13,7 +13,7 @@ import java.math.BigDecimal;
 @Data
 public class Product {
     @Id
-    private int id;
+    private String id;
     private String name;
     private String description;
     private BigDecimal price;

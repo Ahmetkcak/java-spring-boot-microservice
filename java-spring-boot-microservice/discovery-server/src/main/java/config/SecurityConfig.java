13edc8409e46b4a4c351f41e6c39592502681bff
Ahmetkcak <ahmet.aliberk07@gmail.com>
2024-04-14 15:50:13 +0300

    fix:update SecurityConfig in discovery service

--- java-spring-boot-microservice/discovery-server/src/main/java/config/SecurityConfig.java
+++ java-spring-boot-microservice/discovery-server/src/main/java/config/SecurityConfig.java
@@ -1,43 +1,20 @@
 package config;
 
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Value("${eureka.username}")
-    private String username;
-
-    @Value("${eureka.password}")
-    private String password;
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                //Don't use this in production and real projects
-                .passwordEncoder(NoOpPasswordEncoder.getInstance())
-                .withUser(username)
-                .password(password)
-                .authorities("USER");
-
-    }
-
-    @Override
-    protected void configure(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .csrf(AbstractHttpConfigurer::disable)
-                .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
-                .httpBasic(AbstractHttpConfigurer::disable);
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/eureka/**").permitAll()
+                .anyRequest().authenticated()
+        );
+        return httpSecurity.build();
     }
 }
